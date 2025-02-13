48645038250bdd44291c9d5e871543baa911447e
Joshua Scarbrough <159404014+JoshuaScarbrough@users.noreply.github.com>
2025-02-13 12:47:31 -0600

    Update app.py
    Added Waitress

--- app.py
+++ app.py
@@ -479,5 +479,7 @@ def EditToDoTask(username):
 
 """ This what allows our python app to run"""
 if __name__ == '__main__':
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
+    // app.run(debug=True)
     
